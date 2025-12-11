507a461e3b4414d5951ad9fe65c58e50addd1eb0
MarcusSanto <167369960+MarcusSanto@users.noreply.github.com>
2025-12-11 17:52:31 -0300

    iniciando o projeto

---
+++ appBanco/interfaceUser.java
@@ -0,0 +1,5 @@
+package appBanco;
+
+public class interfaceUser {
+
+}
---
+++ contaUsuario/contaBanco.java
@@ -0,0 +1,8 @@
+package contaUsuario;
+
+public class contaBanco {
+    private String name;
+    private int accountNumb;
+    private String CPF;
+    private double saldo;
+}
