42b4029fde9d78c30bb5082708bba0c2f4e1442f
MarcusSanto <167369960+MarcusSanto@users.noreply.github.com>
2026-01-15 19:40:58 -0300

    Adicionando gets & sets a contaBanco

--- contaUsuario/contaBanco.java
+++ contaUsuario/contaBanco.java
@@ -1,8 +1,34 @@
 package contaUsuario;
 
 public class contaBanco {
+
     private String name;
     private int accountNumb;
     private String CPF;
     private double saldo;
+    
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+  public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAccountNumb() {
+        return accountNumb;
+    }
+    public void setAccountNumb(int accountNumb) {
+        this.accountNumb = accountNumb;
+    }  
+    public String getCPF() {
+        return CPF;
+    }
+    public void setCPF(String cPF) {
+        CPF = cPF;
+    }
 }
