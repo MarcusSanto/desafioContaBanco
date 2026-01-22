cd42b1d01fd54bc9e1781349ee3b63c1a1b0dc1a
MarcusSanto <167369960+MarcusSanto@users.noreply.github.com>
2026-01-22 19:51:15 -0300

    adição de funções basicas e interface

--- appBanco/interfaceUser.java
+++ appBanco/interfaceUser.java
@@ -1,5 +1,56 @@
 package appBanco;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class interfaceUser {
 
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao Banco Java!");
+        System.out.print("Digite seu nome: ");
+        String name = sc.nextLine();
+        System.out.print("Digite seu sobrenome: ");
+        String lastName = sc.nextLine();
+        System.out.print("Digite seu CPF: ");
+        String CPF = sc.nextLine();
+
+        contaUsuario.contaBanco conta = new contaUsuario.contaBanco(name, lastName, CPF);
+
+        int opcao;
+        do {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1 - Mostrar dados da conta");
+            System.out.println("2 - Consultar saldo");
+            System.out.println("3 - Depositar valor");
+            System.out.println("4 - Sacar valor");
+            System.out.println("0 - Sair");
+            System.out.print("Opção: ");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1 -> conta.mostrarDadosConta();
+                case 2 -> conta.consultarSaldo();
+                case 3 -> {
+                    System.out.print("Digite o valor para depósito: R$ ");
+                    double deposito = sc.nextDouble();
+                    conta.depositarValor(deposito);
+                }
+                case 4 -> {
+                    System.out.print("Digite o valor para saque: R$ ");
+                    double saque = sc.nextDouble();
+                    conta.sacarValor(saque);
+                }
+                case 0 -> System.out.println("Obrigado por usar o Banco Java. Até logo!");
+                default -> System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (opcao != 0);
+
+        sc.close();
+
+
+
+    }
+        
 }
--- contaUsuario/contaBanco.java
+++ contaUsuario/contaBanco.java
@@ -1,34 +1,59 @@
 package contaUsuario;
 
+/**
+ * @author MarcusSantos
+ */
+
 public class contaBanco {
 
     private String name;
-    private int accountNumb;
-    private String CPF;
+    private String lastName;
+    private final int accountNumb = (int) (Math.random() * 10000);
+    private final String CPF;
     private double saldo;
-    
-    public double getSaldo() {
-        return saldo;
-    }
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-  public String getName() {
-        return name;
-    }
-    public void setName(String name) {
+
+ /**
+     * @param name Nome do usuário
+     * @param lastName Sobrenome do usuário
+     * @param CPF CPF do usuário
+     */
+
+    public contaBanco(String name, String lastName, String CPF) {
         this.name = name;
+        this.lastName = lastName;
+        this.CPF = CPF;
+        this.saldo = 0.0;
     }
-    public int getAccountNumb() {
-        return accountNumb;
+
+    public void mostrarDadosConta(){
+        System.out.println("Nome: " + this.name + " " + this.lastName);
+        System.out.println("CPF: " + this.CPF);
+        System.out.println("Número da Conta: " + this.accountNumb);
+        System.out.println("Saldo: R$ " + String.format("%.2f", this.saldo));
     }
-    public void setAccountNumb(int accountNumb) {
-        this.accountNumb = accountNumb;
-    }  
-    public String getCPF() {
-        return CPF;
+
+    public void consultarSaldo(){
+        System.out.println("\n>> Saldo atual: R$ " + String.format("%.2f", this.saldo));
     }
-    public void setCPF(String cPF) {
-        CPF = cPF;
+
+    public void depositarValor(double valor){
+        if (valor > 0) {
+            this.saldo += valor;
+            System.out.println("\n>> Depósito de R$ " + String.format("%.2f", valor) + " realizado com sucesso!");
+            consultarSaldo();
+        } else {
+            System.out.println("\n>> Valor de depósito inválido. Tente novamente.");
+        }
     }
+
+    public void sacarValor(double valor){
+        if (valor > 0 && valor <= this.saldo) {
+            this.saldo -= valor;
+            System.out.println("\n>> Saque de R$ " + String.format("%.2f", valor) + " realizado com sucesso!");
+            consultarSaldo();
+        } else {
+            System.out.println("\n>> Saldo insuficiente ou valor inválido para saque.");
+        }
+    }
+
 }
